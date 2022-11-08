ade0f38d1d4d14867d852541a5476f994e97421f
Priyansh Bhakuni <94987692+priyansh-bhakuni@users.noreply.github.com>
2022-11-08 19:44:15 +0530

    For connecting to DB

---
+++ dbm.py
@@ -0,0 +1,22 @@
+from pymongo import MongoClient, DESCENDING
+from werkzeug.security import generate_password_hash
+
+from user import User
+
+client = MongoClient("mongodb+srv://test:test@chatapp.bpcztji.mongodb.net/?retryWrites=true&w=majority")
+
+chat_db = client.get_database("ChatDB")
+users_collection = chat_db.get_collection("users")
+rooms_collection = chat_db.get_collection("rooms")
+room_members_collection = chat_db.get_collection("room_members")
+messages_collection = chat_db.get_collection("messages")
+
+
+def save_user(username, email, password):
+    password_hash = generate_password_hash(password)
+    users_collection.insert_one({'_id': username, 'email': email, 'password': password_hash})
+
+
+def get_user(username):
+    user_data = users_collection.find_one({'_id': username})
+    return User(user_data['_id'], user_data['email'], user_data['password']) if user_data else None
